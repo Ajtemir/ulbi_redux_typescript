5ad6df94e434374628c501076d21a92b839aed8a
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2023-10-14 19:03:46 +0600

    создаем хук UseActions

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -3,13 +3,15 @@ import {useDispatch, useSelector} from "react-redux";
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {fetchUsers} from "../store/action-creators/user";
 import {UserAction} from "../types/todo";
+import {useActions} from "../hooks/useActions";
 
 const UserList : React.FC = () => {
     const {users, loading, error} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions()
     const dispatch = useDispatch()
     useEffect(() => {
         // @ts-ignore
-        dispatch(fetchUsers())
+        fetchUsers()
     }, [])
 
 
